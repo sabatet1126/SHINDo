522c7ecd69d366c0282a7312f22ad076a5accac4
藤田 麻里 <marimari20001126@docomo.ne.jp>
2017-01-10 20:15:43 +0900

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by 藤田麻里 on 2017/01/10.
+//  Copyright © 2017年 鯖. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
